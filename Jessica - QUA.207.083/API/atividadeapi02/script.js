f7b39b83377514955d2b58e52070bfbd2666fa18
JessicaSousa26 <jhessymary26@gmail.com>
2025-10-15 17:48:26 -0300

    Aula 2 API
    Atividade 02 de API

---
+++ Jessica - QUA.207.083/API/atividadeapi02/script.js
@@ -0,0 +1,13 @@
+function gerarCachorro() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const img = document.getElementById("dogImg");
+        img.src = data.message;
+    })
+    .catch(error => {
+        console.error("Erro ao buscar cachorro:", error);
+        alert("Ocorreu um erro ao carregar o cachorro");
+    });
+}
